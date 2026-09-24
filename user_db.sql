f1798178d5aa5d95cbee280666f3efc0f6ea1433
lumbodaniel861-ux <lumbodaniel861@gmail.com>
2026-09-24 10:27:38 +0800

    Add files via upload

---
+++ user_db.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2026 at 04:11 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_information`
+--
+
+CREATE TABLE `customer_information` (
+  `First_Name` varchar(50) NOT NULL,
+  `Middle_Name` varchar(50) NOT NULL,
+  `Last_Name` varchar(50) NOT NULL,
+  `Birth_date` date NOT NULL,
+  `Gender` varchar(10) NOT NULL,
+  `Email` text NOT NULL,
+  `Phone_Number` int(20) NOT NULL,
+  `Address` text NOT NULL,
+  `Username` text NOT NULL,
+  `Password` text NOT NULL,
+  `User_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_db`
+--
+
+CREATE TABLE `employee_db` (
+  `Employee_ID` int(11) NOT NULL,
+  `First_name` varchar(50) NOT NULL,
+  `Middle_name` varchar(50) NOT NULL,
+  `Last_name` varchar(50) NOT NULL,
+  `Gender` varchar(255) NOT NULL,
+  `Email` text NOT NULL,
+  `Phone_Number` int(20) NOT NULL,
+  `Address` text NOT NULL,
+  `Username` text NOT NULL,
+  `Password` text NOT NULL,
+  `Department` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer_information`
+--
+ALTER TABLE `customer_information`
+  ADD PRIMARY KEY (`User_ID`);
+
+--
+-- Indexes for table `employee_db`
+--
+ALTER TABLE `employee_db`
+  ADD PRIMARY KEY (`Employee_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer_information`
+--
+ALTER TABLE `customer_information`
+  MODIFY `User_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employee_db`
+--
+ALTER TABLE `employee_db`
+  MODIFY `Employee_ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
